2cf5d5b50847c360648328b10dee55a7ec079b2d
Chris Young <cyoung@architectnow.net>
2017-09-13 10:28:43 -0500

    Fixed saveasync with empty guid

--- src/ArchitectNow.Mongo/Db/BaseRepository.cs
+++ src/ArchitectNow.Mongo/Db/BaseRepository.cs
@@ -15,285 +15,288 @@ using MongoDB.Driver;
 
 namespace ArchitectNow.Mongo.Db
 {
-	//Test notifications
-	public abstract class BaseRepository<TModel, TDataContext> : IBaseRepository<TModel> where TModel : BaseDocument where TDataContext: MongoDataContext
-	{
-		private readonly IValidator<TModel> _validator;
+    //Test notifications
+    public abstract class BaseRepository<TModel, TDataContext> : IBaseRepository<TModel> where TModel : BaseDocument
+        where TDataContext : MongoDataContext
+    {
+        private readonly IValidator<TModel> _validator;
 
-		private IMongoCollection<TModel> _collection;
+        private IMongoCollection<TModel> _collection;
 
-		private string _regionName = "";
+        private string _regionName = "";
 
-		private string _searchRegionName = "";
-		
-		protected BaseRepository(ILogger<TModel> logger, IMongoDbUtilities mongoDbUtilities,
-			IDataContextService<TDataContext> dataContextService, ICacheService cacheService, IValidator<TModel> validator = null)
-		{
-			_validator = validator ?? new InlineValidator<TModel>();
+        private string _searchRegionName = "";
 
-			CacheService = cacheService;
-			CurrentContext = dataContextService.GetDataContext();
-			Logger = logger;
-			DbUtilities = mongoDbUtilities;
-		}
+        protected BaseRepository(ILogger<TModel> logger, IMongoDbUtilities mongoDbUtilities,
+            IDataContextService<TDataContext> dataContextService, ICacheService cacheService,
+            IValidator<TModel> validator = null)
+        {
+            _validator = validator ?? new InlineValidator<TModel>();
 
-		protected ICacheService CacheService { get; }
-		protected TDataContext CurrentContext { get; }
-		protected ILogger<TModel> Logger { get; }
-		protected IMongoDbUtilities DbUtilities { get; }
+            CacheService = cacheService;
+            CurrentContext = dataContextService.GetDataContext();
+            Logger = logger;
+            DbUtilities = mongoDbUtilities;
+        }
 
-		/// <summary>
-		///     Gets the data query.
-		/// </summary>
-		/// <value>
-		///     The data query.
-		/// </value>
-		protected virtual IQueryable<TModel> DataQuery
-		{
-			get
-			{
-				if (_collection == null)
-					_collection = DbUtilities.Database.GetCollection<TModel>(CollectionName);
+        protected ICacheService CacheService { get; }
+        protected TDataContext CurrentContext { get; }
+        protected ILogger<TModel> Logger { get; }
+        protected IMongoDbUtilities DbUtilities { get; }
 
-				return _collection.AsQueryable();
-			}
-		}
+        /// <summary>
+        ///     Gets the data query.
+        /// </summary>
+        /// <value>
+        ///     The data query.
+        /// </value>
+        protected virtual IQueryable<TModel> DataQuery
+        {
+            get
+            {
+                if (_collection == null)
+                    _collection = DbUtilities.Database.GetCollection<TModel>(CollectionName);
 
-		/// <summary>
-		///     Gets the name of the region.
-		/// </summary>
-		/// <value>
-		///     The name of the region.
-		/// </value>
-		public string RegionName
-		{
-			get
-			{
-				if (string.IsNullOrEmpty(_regionName))
-				{
-					_regionName = BuildCacheKeyPrefix().Replace(".", "");
-					CacheService.CreateRegion(_regionName);
-				}
-				return _regionName;
-			}
-		}
+                return _collection.AsQueryable();
+            }
+        }
 
-		/// <summary>
-		///     Gets the name of the search region.
-		/// </summary>
-		/// <value>
-		///     The name of the search region.
-		/// </value>
-		public string SearchRegionName
-		{
-			get
-			{
-				if (string.IsNullOrEmpty(_searchRegionName))
-				{
-					_searchRegionName = RegionName + ".Searches".Replace(".", "");
-					CacheService.CreateRegion(_searchRegionName);
-				}
+        /// <summary>
+        ///     Gets the name of the region.
+        /// </summary>
+        /// <value>
+        ///     The name of the region.
+        /// </value>
+        public string RegionName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_regionName))
+                {
+                    _regionName = BuildCacheKeyPrefix().Replace(".", "");
+                    CacheService.CreateRegion(_regionName);
+                }
+                return _regionName;
+            }
+        }
 
-				return _searchRegionName;
-			}
-		}
+        /// <summary>
+        ///     Gets the name of the search region.
+        /// </summary>
+        /// <value>
+        ///     The name of the search region.
+        /// </value>
+        public string SearchRegionName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_searchRegionName))
+                {
+                    _searchRegionName = RegionName + ".Searches".Replace(".", "");
+                    CacheService.CreateRegion(_searchRegionName);
+                }
 
-		/// <summary>
-		///     Determines whether [has valid user].
-		/// </summary>
-		/// <returns></returns>
-		public bool HasValidUser()
-		{
-			return CurrentContext?.CurrentUserId != null && CurrentContext.CurrentUserId != Guid.Empty;
-		}
+                return _searchRegionName;
+            }
+        }
 
-		public abstract string CollectionName { get; }
+        /// <summary>
+        ///     Determines whether [has valid user].
+        /// </summary>
+        /// <returns></returns>
+        public bool HasValidUser()
+        {
+            return CurrentContext?.CurrentUserId != null && CurrentContext.CurrentUserId != Guid.Empty;
+        }
 
-		/// <summary>
-		///     Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-		/// </summary>
-		public virtual void Dispose()
-		{
-		}
+        public abstract string CollectionName { get; }
 
-		public virtual async Task<bool> DeleteAllAsync()
-		{
-			CacheService.ClearRegion(RegionName);
-			CacheService.ClearRegion(SearchRegionName);
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public virtual void Dispose()
+        {
+        }
 
-			var filter = new BsonDocument();
-			await GetCollection().DeleteManyAsync(filter);
+        public virtual async Task<bool> DeleteAllAsync()
+        {
+            CacheService.ClearRegion(RegionName);
+            CacheService.ClearRegion(SearchRegionName);
 
-			return true;
-		}
+            var filter = new BsonDocument();
+            await GetCollection().DeleteManyAsync(filter);
 
-		public virtual async Task<List<TModel>> GetAllAsync(bool onlyActive = true)
-		{
-			var cacheKey = BuildCacheKey(nameof(GetAllAsync), onlyActive);
+            return true;
+        }
 
-			var results = CacheService.Get<List<TModel>>(cacheKey, SearchRegionName);
-			if (results != null)
-				return results;
+        public virtual async Task<List<TModel>> GetAllAsync(bool onlyActive = true)
+        {
+            var cacheKey = BuildCacheKey(nameof(GetAllAsync), onlyActive);
 
-			if (onlyActive)
-				results = await GetCollection().Find(x => x.IsActive).ToListAsync();
-			else
-				results = await GetCollection().Find(x => x.IsActive).ToListAsync();
+            var results = CacheService.Get<List<TModel>>(cacheKey, SearchRegionName);
+            if (results != null)
+                return results;
 
-			CacheService.Add(cacheKey, results, SearchRegionName);
-			return results;
-		}
+            if (onlyActive)
+                results = await GetCollection().Find(x => x.IsActive).ToListAsync();
+            else
+                results = await GetCollection().Find(x => x.IsActive).ToListAsync();
 
-		public virtual async Task<TModel> GetOneAsync(Guid id)
-		{
-			var cacheKey = BuildCacheKey(nameof(GetOneAsync), id);
+            CacheService.Add(cacheKey, results, SearchRegionName);
+            return results;
+        }
 
-			var result = CacheService.Get<TModel>(cacheKey, RegionName);
+        public virtual async Task<TModel> GetOneAsync(Guid id)
+        {
+            var cacheKey = BuildCacheKey(nameof(GetOneAsync), id);
 
-			if (result != null)
-				return result;
+            var result = CacheService.Get<TModel>(cacheKey, RegionName);
 
-			result = await GetCollection().Find(x => x.Id == id).FirstOrDefaultAsync();
+            if (result != null)
+                return result;
 
-			CacheService.Add(cacheKey, result, RegionName);
-			return result;
-		}
+            result = await GetCollection().Find(x => x.Id == id).FirstOrDefaultAsync();
 
-		public virtual async Task<TModel> SaveAsync(TModel item)
-		{
-			if (item.Id != Guid.Empty)
-				item.UpdatedDate = DateTime.UtcNow;
+            CacheService.Add(cacheKey, result, RegionName);
+            return result;
+        }
 
-			if (item.OwnerUserId == null || item.OwnerUserId == Guid.Empty)
-				if (CurrentContext != null && CurrentContext.CurrentUserId != Guid.Empty)
-					item.OwnerUserId = CurrentContext.CurrentUserId;
+        public virtual async Task<TModel> SaveAsync(TModel item)
+        {
+            if (item.Id != Guid.Empty)
+                item.UpdatedDate = DateTime.UtcNow;
 
-			var errors = await ValidateObject(item);
+            if (item.OwnerUserId == null || item.OwnerUserId == Guid.Empty)
+                if (CurrentContext != null && CurrentContext.CurrentUserId != Guid.Empty)
+                    item.OwnerUserId = CurrentContext.CurrentUserId;
 
-			if (errors.Any())
-				throw new ValidationException("A validation error has occured saving item of type '" + item.GetType(), errors);
+            var errors = await ValidateObject(item);
 
-			var existing = await GetOneAsync(item.Id);
+            if (errors.Any())
+                throw new ValidationException("A validation error has occured saving item of type '" + item.GetType(),
+                    errors);
 
-			if (existing == null)
-			{
-				await GetCollection().InsertOneAsync(item);
-			}
-			else
-			{
-				var filter = Builders<TModel>.Filter.Eq("_id", item.Id);
-				await GetCollection().ReplaceOneAsync(filter, item);
-			}
+            if (item.Id == Guid.Empty)
+            {
+                item.Id = Guid.NewGuid();
+                await GetCollection().InsertOneAsync(item);
+            }
+            else
+            {
+                var filter = Builders<TModel>.Filter.Eq("_id", item.Id);
+                await GetCollection().ReplaceOneAsync(filter, item, new UpdateOptions {IsUpsert = true});
+            }
 
-			Logger.LogInformation(EventIds.Update, "Entity Saved to {CollectionName}: \'{Id}\' - {@item}", CollectionName,
-				item.Id, item);
+            Logger.LogInformation(EventIds.Update, "Entity Saved to {CollectionName}: \'{Id}\' - {@item}",
+                CollectionName,
+                item.Id, item);
 
-			//make sure we update the cache...
-			var cacheKey = BuildCacheKey(nameof(GetOneAsync), item.Id);
+            //make sure we update the cache...
+            var cacheKey = BuildCacheKey(nameof(GetOneAsync), item.Id);
 
-			CacheService.Add(cacheKey, item, RegionName);
+            CacheService.Add(cacheKey, item, RegionName);
 
-			CacheService.ClearRegion(SearchRegionName);
-			return item;
-		}
+            CacheService.ClearRegion(SearchRegionName);
+            return item;
+        }
 
-		public virtual async Task<bool> DeleteAsync(Guid id)
-		{
-			var filter = Builders<TModel>.Filter.Eq("_id", id);
+        public virtual async Task<bool> DeleteAsync(Guid id)
+        {
+            var filter = Builders<TModel>.Filter.Eq("_id", id);
 
-			await GetCollection().DeleteOneAsync(filter);
+            await GetCollection().DeleteOneAsync(filter);
 
-			Logger.LogInformation(EventIds.Update, "Entity Deleted to {CollectionName}: \'{id}\'", CollectionName, id);
+            Logger.LogInformation(EventIds.Update, "Entity Deleted to {CollectionName}: \'{id}\'", CollectionName, id);
 
-			var cacheKey = BuildCacheKey("GetOne", id);
+            var cacheKey = BuildCacheKey("GetOne", id);
 
-			CacheService.Remove(cacheKey, RegionName);
+            CacheService.Remove(cacheKey, RegionName);
 
-			CacheService.ClearRegion(SearchRegionName);
-			return true;
-		}
+            CacheService.ClearRegion(SearchRegionName);
+            return true;
+        }
 
-		public virtual async Task<bool> DeleteAsync(TModel item)
-		{
-			var cacheKey = BuildCacheKey(nameof(GetOneAsync), item.Id);
-			CacheService.Remove(cacheKey, RegionName);
+        public virtual async Task<bool> DeleteAsync(TModel item)
+        {
+            var cacheKey = BuildCacheKey(nameof(GetOneAsync), item.Id);
+            CacheService.Remove(cacheKey, RegionName);
 
-			return await DeleteAsync(item.Id);
-		}
+            return await DeleteAsync(item.Id);
+        }
 
-		/// <summary>
-		///     Configures the indexes.
-		/// </summary>
-		public virtual async Task ConfigureIndexes()
-		{
-			await CreateIndex("Id", Builders<TModel>.IndexKeys.Ascending(x => x.Id).Ascending(x => x.IsActive));
-		}
+        /// <summary>
+        ///     Configures the indexes.
+        /// </summary>
+        public virtual async Task ConfigureIndexes()
+        {
+            await CreateIndex("Id", Builders<TModel>.IndexKeys.Ascending(x => x.Id).Ascending(x => x.IsActive));
+        }
 
-		/// <summary>
-		///     Gets the collection.
-		/// </summary>
-		/// <returns></returns>
-		protected IMongoCollection<TModel> GetCollection()
-		{
-			return _collection ?? (_collection = DbUtilities.Database.GetCollection<TModel>(CollectionName));
-		}
+        /// <summary>
+        ///     Gets the collection.
+        /// </summary>
+        /// <returns></returns>
+        protected IMongoCollection<TModel> GetCollection()
+        {
+            return _collection ?? (_collection = DbUtilities.Database.GetCollection<TModel>(CollectionName));
+        }
 
 
-		/// <summary>
-		///     Builds the cache key prefix.
-		/// </summary>
-		/// <returns></returns>
-		protected virtual string BuildCacheKeyPrefix()
-		{
-			return $"{GetType()}.{CurrentContext?.CurrentOrganizationId}";
-		}
+        /// <summary>
+        ///     Builds the cache key prefix.
+        /// </summary>
+        /// <returns></returns>
+        protected virtual string BuildCacheKeyPrefix()
+        {
+            return $"{GetType()}.{CurrentContext?.CurrentOrganizationId}";
+        }
 
-		/// <summary>
-		///     Builds the cache key.
-		/// </summary>
-		/// <param name="methodName">Name of the method.</param>
-		/// <param name="parameters">The parameters.</param>
-		/// <returns></returns>
-		protected virtual string BuildCacheKey(string methodName, params object[] parameters)
-		{
-			var key = $"{BuildCacheKeyPrefix()}.{methodName}";
+        /// <summary>
+        ///     Builds the cache key.
+        /// </summary>
+        /// <param name="methodName">Name of the method.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns></returns>
+        protected virtual string BuildCacheKey(string methodName, params object[] parameters)
+        {
+            var key = $"{BuildCacheKeyPrefix()}.{methodName}";
 
-			if (parameters != null && parameters.Length > 0)
-				key = parameters.Aggregate(key, (current, param) => current + (".-" + param));
+            if (parameters != null && parameters.Length > 0)
+                key = parameters.Aggregate(key, (current, param) => current + (".-" + param));
 
-			return key;
-		}
+            return key;
+        }
 
-		protected virtual async Task CreateIndex(string name, IndexKeysDefinition<TModel> keys)
-		{
-			var options = new CreateIndexOptions<TModel>
-			{
-				Name = name
-			};
+        protected virtual async Task CreateIndex(string name, IndexKeysDefinition<TModel> keys)
+        {
+            var options = new CreateIndexOptions<TModel>
+            {
+                Name = name
+            };
 
-			await GetCollection().Indexes.CreateOneAsync(keys, options);
-		}
+            await GetCollection().Indexes.CreateOneAsync(keys, options);
+        }
 
-		/// <summary>
-		///     Validates the object.
-		/// </summary>
-		/// <param name="item">The item.</param>
-		/// <returns></returns>
-		protected virtual async Task<IList<ValidationFailure>> ValidateObject(TModel item)
-		{
-			var validationResult = await _validator.ValidateAsync(item);
-			var validationResultErrors = validationResult.Errors;
-			return validationResultErrors;
-		}
+        /// <summary>
+        ///     Validates the object.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns></returns>
+        protected virtual async Task<IList<ValidationFailure>> ValidateObject(TModel item)
+        {
+            var validationResult = await _validator.ValidateAsync(item);
+            var validationResultErrors = validationResult.Errors;
+            return validationResultErrors;
+        }
 
-		protected virtual async Task<long> CountAsync(Expression<Func<TModel, bool>> filter)
-		{
-			return await GetCollection().CountAsync(filter);
-		}
+        protected virtual async Task<long> CountAsync(Expression<Func<TModel, bool>> filter)
+        {
+            return await GetCollection().CountAsync(filter);
+        }
 
-		protected async Task<List<TModel>> FindAsync(Expression<Func<TModel, bool>> filter)
-		{
-			return await GetCollection().Find(filter).ToListAsync();
-		}
-	}
+        protected async Task<List<TModel>> FindAsync(Expression<Func<TModel, bool>> filter)
+        {
+            return await GetCollection().Find(filter).ToListAsync();
+        }
+    }
 }
