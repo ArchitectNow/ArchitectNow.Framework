08c2099835841b24d5969bc053b2562700599df1
Chris Young <cyoung@architectnow.net>
2018-05-22 14:52:05 -0500

    Added SwaggerOptions back

---
+++ src/ArchitectNow.Web/Configuration/SwaggerExtensions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ArchitectNow.Web.Models;
+using Microsoft.AspNetCore.Builder.Internal;
+using NJsonSchema;
+using NSwag.AspNetCore;
+using NSwag.SwaggerGeneration.WebApi;
+
+namespace ArchitectNow.Web.Configuration
+{
+    public static class SwaggerExtensions
+    {
+        public static void ConfigureSwaggerUi3(this ApplicationBuilder builder, IEnumerable<SwaggerOptions> options, Action<SwaggerUi3Settings<WebApiToSwaggerGeneratorSettings>> configure)
+        {
+            foreach (var option in options)
+            {
+
+                if (option.Controllers?.Any() == true)
+                {
+                    builder.UseSwaggerUi3(option.Controllers, settings => { ConfigureSettings(settings, configure, option); });
+                }
+                else
+                {
+                    builder.UseSwaggerUi3(option.ControllerAssembly, settings => { ConfigureSettings(settings, configure, option); });
+                }
+            }
+        }
+        
+        public static void ConfigureSwaggerUi(this ApplicationBuilder builder, IEnumerable<SwaggerOptions> options, Action<SwaggerUiSettings<WebApiToSwaggerGeneratorSettings>> configure)
+        {
+            foreach (var option in options)
+            {
+
+                if (option.Controllers?.Any() == true)
+                {
+                    builder.UseSwaggerUi(option.Controllers, settings => { ConfigureSettings(settings, configure, option); });
+                }
+                else
+                {
+                    builder.UseSwaggerUi(option.ControllerAssembly, settings => { ConfigureSettings(settings, configure, option); });
+                }
+            }
+        }
+        
+        public static void ConfigureSwaggerReDoc(this ApplicationBuilder builder, IEnumerable<SwaggerOptions> options, Action<SwaggerReDocSettings<WebApiToSwaggerGeneratorSettings>> configure)
+        {
+            foreach (var option in options)
+            {
+
+                if (option.Controllers?.Any() == true)
+                {
+                    builder.UseSwaggerReDoc(option.Controllers, settings => { ConfigureSettings(settings, configure, option); });
+                }
+                else
+                {
+                    builder.UseSwaggerReDoc(option.ControllerAssembly, settings => { ConfigureSettings(settings, configure, option); });
+                }
+            }
+        }
+
+        private static void ConfigureSettings<T>(T settings, Action<T> configure, SwaggerOptions option) where T: SwaggerUiSettingsBase<WebApiToSwaggerGeneratorSettings>
+        {
+            settings.SwaggerRoute = option.SwaggerRoute;
+            settings.SwaggerUiRoute = option.SwaggerUiRoute;
+            settings.GeneratorSettings.DefaultPropertyNameHandling = PropertyNameHandling.CamelCase;
+            settings.GeneratorSettings.Title = option.Title;
+            settings.GeneratorSettings.FlattenInheritanceHierarchy = true;
+            settings.GeneratorSettings.IsAspNetCore = true;
+
+            foreach (var optionDocumentProcessor in option.DocumentProcessors)
+            {
+                settings.GeneratorSettings.DocumentProcessors.Add(optionDocumentProcessor);
+            }
+
+            foreach (var operationProcessor in option.OperationProcessors)
+            {
+                settings.GeneratorSettings.OperationProcessors.Add(operationProcessor);
+            }
+            
+            configure?.Invoke(settings);
+        }
+    }
+}
---
+++ src/ArchitectNow.Web/Models/SwaggerOptions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using NSwag;
+using NSwag.SwaggerGeneration.Processors;
+using NSwag.SwaggerGeneration.Processors.Security;
+
+namespace ArchitectNow.Web.Models
+{
+    public class SwaggerOptions
+    {
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public readonly string DefaultSwaggerRoute = "/docs/v1/swagger.json";
+
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public readonly string DefaultSwaggerUiRoute = "/docs";
+	    
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string SwaggerRoute { get; set; } = "/docs/v1/swagger.json";
+        public string SwaggerUiRoute { get; set; } = "/docs";
+        public IEnumerable<Type> Controllers { get; set; }
+        public Assembly ControllerAssembly { get; set; } = Assembly.GetEntryAssembly();
+//        public Action<SwaggerUiSettingsBase<WebApiToSwaggerGeneratorSettings>> Configure { get; set; }
+	    
+        public IList<IDocumentProcessor> DocumentProcessors { get; set; } = new List<IDocumentProcessor>();
+	    
+        [SuppressMessage("ReSharper", "CollectionNeverUpdated.Global")]
+        public IList<IOperationProcessor> OperationProcessors { get; set; } = new List<IOperationProcessor>();
+	    
+        public SwaggerOptions(bool includeAuthorizationHeader = true)
+        {
+            if (includeAuthorizationHeader)
+            {
+                DocumentProcessors.Add(
+                    new SecurityDefinitionAppender("Authorization", new SwaggerSecurityScheme
+                    {
+                        Type = SwaggerSecuritySchemeType.ApiKey,
+                        Name = "Authorization",
+                        In = SwaggerSecurityApiKeyLocation.Header
+                    })
+                );
+            }
+        }
+    }
+}
